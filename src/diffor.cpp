54924c72deb098aaaf28abb64254ef9bdf2fa782
GBobkov <bobkoveu@gmail.com>
2024-11-25 23:05:56 +0300

    25.11 иду спать

--- src/diffor.cpp
+++ src/diffor.cpp
@@ -4,34 +4,44 @@
 #include <stdio.h>
 
 
+#FIXME: Исправить фигню со связями в графе. аккуратнее с возвращающим узлом и созданием узла (проследить за уникальностью узла.) 
 // Рекурсивная функция вычисления производной
 static NODE* New_Step_Drvt(NODE* node)
 {
 
-    if (node->type = NUM_DATA)
+    if (node->type == NUM_DATA)
     {
         node->data = 0;
         return node;
     }
-    if (node->type = OP_DATA)
+    if (node->type == VAR_DATA)
     {
-        switch (node->data)
+        node->type = NUM_DATA;
+        node->data = 0;
+        return node;
+    }
+    if (node->type == OP_DATA)
+    {
+        printf("node->data=%d, node->type=%d\n", node->data, node->type);
+        if (node->data == '+' || node->data == '-')
         {
-        case '+':
-        case '-':
             node->left = New_Step_Drvt(node->left);
             node->right = New_Step_Drvt(node->right);
             return node;
-        
-        case '*':
-            {
-                node->data = '+';
-                NODE* left = node->left;
-                NODE* right = node->right;
-                node->left = Create_Node(OP_DATA, '*', node, New_Step_Drvt(left), right);
-                node->right = Create_Node(OP_DATA, '*', node, left, New_Step_Drvt(right));
-            }
-        case '^':
+        }
+        if (node->data == '*')
+        {
+            printf("Ti pidoras!\n");
+            node->data = '+';
+            NODE* left = node->left;
+            NODE* right = node->right;
+            node->left = Create_Node(OP_DATA, '*', node, New_Step_Drvt(left), right);
+            node->right = Create_Node(OP_DATA, '*', node, left, New_Step_Drvt(right));
+            printf("Suuuda nahui!\n");
+            return Create_Node(node->type, node->data, node->parent, node->left, node->right);
+        }
+        if (node->data == '^')
+        {
             if (node->left->type == NUM_DATA)
             {
                 node->type = NUM_DATA;
@@ -40,6 +50,7 @@ static NODE* New_Step_Drvt(NODE* node)
                 Destroy_Tree(node->right);
                 node->left = NULL;
                 node->right = NULL;
+                return Create_Node(node->type, node->data, node->parent, node->left, node->right);
             }
             else if (node->left->type == VAR_DATA && node->right->type == NUM_DATA)
             {
@@ -47,11 +58,11 @@ static NODE* New_Step_Drvt(NODE* node)
                 Destroy_Tree(node->left);
                 node->left = Create_Node(NUM_DATA, node->right->data, node, NULL, NULL);
                 Destroy_Tree(node->right);
-                node->right = Create_Node(OP_DATA, '*', node, Create_Node(VAR_DATA, 'x', NULL, NULL, NULL), Create_Node(NUM_DATA, node->left->data, NULL, NULL, NULL));
+                node->right = Create_Node(OP_DATA, '*', node, Create_Node(VAR_DATA, 'x', NULL, NULL, NULL), Create_Node(NUM_DATA, node->left->data - 1, NULL, NULL, NULL));
+                return Create_Node(node->type, node->data, node->parent, node->left, node->right);
             }
-        default:
-            break;
         }
+        
     }
     
 }
@@ -62,7 +73,8 @@ void Calculate_Derivative(void)
 {
     NODE* head = Create_Node(NONE_DATA, '\0', NULL, NULL, NULL);
     Read_Data(head);
-    New_Step_Drvt(head);
+    Node_Dump("dump.dot", head);
+    head = New_Step_Drvt(head);
     Write_Data(head);
     Node_Dump("dump.dot", head);
 }
